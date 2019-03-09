b23e16596509a11627588c3ffc2fb284fb374324
Lei <lynnl.yet@gmail.com>
2019-03-09 12:11:09 +0800

    Minor refine for lpm.h

--- lpm.h
+++ lpm.h
@@ -14,6 +14,28 @@
  * @tree        the tree
  * @key         key to match
  * @len         length of the key
+ * @pos         [OUT] how many bytes processed(nullable)
+ *              ranged [0, len]
+ *              0           indicate mismatch(will return raxNotFound)
+ *              (0, len)    indicate a submatch
+ *              len         indicate a full match
+ * @return      value associated to the longest prefix match node
+ *              raxNotFound if not match
+ *
+ * see: rax/rax.c#raxFind
+ */
+void *raxLongestPrefixMatch(
+    rax *tree,
+    unsigned char *key,
+    size_t len,
+    size_t *pos);
+
+/**
+ * Find longest prefix match in a radix tree
+ *
+ * @tree        the tree
+ * @key         key to match
+ * @len         length of the key
  * @pos         [OUT] how many bytes processed
  *              ranged [0, len]
  *              0           indicate mismatch(will return raxNotFound)
@@ -30,28 +52,6 @@ int raxLongestPrefixMatch2(
         size_t len,
         size_t * __nullable pos,
         void * __nullable * __nullable data);
-
-/**
- * Find longest prefix match in a radix tree
- *
- * @tree        the tree
- * @key         key to match
- * @len         length of the key
- * @pos         [OUT] how many bytes processed(nullable)
- *              ranged [0, len]
- *              0           indicate mismatch(will return raxNotFound)
- *              (0, len)    indicate a submatch
- *              len         indicate a full match
- * @return      value associated to the longest prefix match node
- *              raxNotFound if not match
- *
- * see: rax/rax.c#raxFind
- */
-void *raxLongestPrefixMatch(
-    rax *tree,
-    unsigned char *key,
-    size_t len,
-    size_t *pos);
 
 #endif
 
